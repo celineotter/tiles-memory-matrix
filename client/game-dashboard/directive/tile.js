e6db8ab39f3a4b2b2378fb32875619e0634989f6
celineotter <celine.otter@gmail.com>
2015-05-25 04:41:08 -0700

    (refactor) Transfer tile directive out of controller page

---
+++ client/game-dashboard/directive/tile.js
@@ -0,0 +1,31 @@
+angular.module('memoryMatrixApp')
+.directive('tile', function () {
+	return {
+	    restrict: 'E',
+		scope: {
+            tile: '=',
+            success: '=',
+            reveal: '=',
+            timer: '='
+        },
+        templateUrl: 'game-dashboard/directive/tile.html',
+        link: function ($scope, element, attrs) {
+
+            $scope.highlightTile = function() {
+
+                if ($scope.timer.active) return;
+
+				if ($scope.tile.secretSelected === false) {
+					$scope.tile.isIncorrect = true;
+                    $scope.reveal(false);
+				} else if($scope.success.counter === 8) {
+					$scope.tile.flash = true;
+                    $scope.reveal(true);
+				} else {
+					$scope.success.counter++;
+					$scope.tile.flash = true;
+				}
+			};
+        }
+	};
+});

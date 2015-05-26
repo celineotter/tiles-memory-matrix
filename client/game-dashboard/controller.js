d810ac0935ce83be1a8d3c0082647dda4c168b8a
celineotter <celine.otter@gmail.com>
2015-05-25 17:42:27 -0700

    (refactor) Transfer Game Class out of controller into factory

--- client/game-dashboard/controller.js
+++ client/game-dashboard/controller.js
@@ -1,91 +1,33 @@
 angular.module('memoryMatrixApp')
-.controller('gameDashboardCtrl', function ($scope, $interval) {
+.controller('gameDashboardCtrl', function ($scope, $interval, gameDashboardFactory) {
 
 	/******************************/
-	/* Shared with tile directive */
+	/* Functionalities shared with tile directive */
 	$scope.tileStatusList = [];
+
 	$scope.success  = {counter: 0};
-    $scope.timer = {active: false};
 
-	/**************************/
-	/* Game controller methods*/
-	var MemoryGame = function () {
-		this._total = 25;
-    	this._timeLeft = 5;
-		this.message = {
-			success: "- Good Job -",
-			fail: "- Better luck next time -",
-			init: "- Start over -",
-			restart: "- Start over -"
-		};
-    	this.secretSelectList = [];
-	};
+	$scope.timer = {active: false};
 
-    /* Populate tileStatusList on first load*/
-    /* Each index represents the initial tile status in matrix*/
-	MemoryGame.prototype.prepareNewGame = function () {
-        this.secretSelectList = [];
-		$scope.success.counter = 0;
+    $scope.revealSuccess = function (success) {
+        if (success) {
+			$scope.userMessage = game.message.success;
+		} else {
+			$scope.userMessage = game.message.fail;
+			var index;
 
-        for (var i=0; i < this._total; i++) {
-            $scope.tileStatusList[i] = $scope.tileStatusList[i] || {};
-
-            $scope.tileStatusList[i].secretSelected = false;
-            $scope.tileStatusList[i].flash = false;
-            $scope.tileStatusList[i].isIncorrect = false;
-            $scope.tileStatusList[i].isRevealed = false;
-        }
+			for (var i=0; i<9; i++) {
+				index = game.secretSelectionList[i];
+				if (!$scope.tileStatusList[index].flash) {
+					$scope.tileStatusList[index].isRevealed = true;
+				}
+			}
+		}
     };
 
-	MemoryGame.prototype._getRandomTile = function () {
-        return Math.floor( Math.random() * this._total);
-    };
-
-	MemoryGame.prototype.generateRandomTiles = function () {
-        var target;
-
-        for (var i=0; i<9; i++) {
-
-            while (!target || this.secretSelectList.indexOf(target) !== -1) {
-                target = this._getRandomTile();
-            }
-
-            this.secretSelectList.push(target);
-            $scope.tileStatusList[target].secretSelected = true;
-        }
-    };
-
-	/***************************************/
-    /* Go through internally selected list */
-    /* For each index value, temporarily flash associated tile */
-    /* Once revealed, cancel their flash parameter */
-	MemoryGame.prototype.revealThenHideSelected = function () {
-        var index = 0, preSelectedIndex;
-
-        while (index < this.secretSelectList.length) {
-            preSelectedIndex = this.secretSelectList[index];
-            $scope.tileStatusList[preSelectedIndex].flash = true;
-            index++;
-        }
-
-		$interval((function (numb) {
-            if (this._timeLeft !== 1) {
-                $scope.userMessage = "- " + --this._timeLeft + " seconds left -";
-            } else {
-	            while (index > 0) {
-	                index--;
-	                preSelectedIndex = this.secretSelectList[index];
-	                $scope.tileStatusList[preSelectedIndex].flash = false;
-	            }
-
-                $scope.userMessage = "- Start your selections now - ";
-                $scope.timer.active = false;
-                this._timeLeft = 5;
-            }
-        }).bind(this), 1000, 5);
-    };
-
-	var game = new MemoryGame();
+	/***************************/
+	/* Extend scope with functionalities not bound to view*/
+	var game = new gameDashboardFactory($scope);
 	game.prepareNewGame();
 
 
@@ -101,25 +43,10 @@ angular.module('memoryMatrixApp')
         game.revealThenHideSelected();
     };
 
-    $scope.endRound = function () {
+	$scope.endRound = function () {
         if ($scope.timer.active) return;
 		game.prepareNewGame();
         $scope.userMessage = game.message.restart;
     };
 
-    $scope.revealSuccess = function (success) {
-        if (success) {
-			$scope.userMessage = game.message.success;
-		} else {
-			$scope.userMessage = game.message.fail;
-			var index;
-
-			for (var i=0; i<9; i++) {
-				index = game.secretSelectList[i];
-				if (!$scope.tileStatusList[index].flash) {
-					$scope.tileStatusList[index].isRevealed = true;
-				}
-			}
-		}
-    };
 });

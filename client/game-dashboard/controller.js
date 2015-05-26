e5cd7200b6b0de3ab603321efbd04bf4ebbebc7b
celineotter <celine.otter@gmail.com>
2015-05-26 00:39:29 -0700

    (refactor) Update controller & view  with Game + Tile services

--- client/game-dashboard/controller.js
+++ client/game-dashboard/controller.js
@@ -1,52 +1,6 @@
 angular.module('memoryMatrixApp')
-.controller('gameDashboardCtrl', function ($scope, $interval, gameDashboardFactory) {
+.controller('gameDashboardCtrl', function ($scope, Game) {
 
-	/******************************/
-	/* Functionalities shared with tile directive */
-	$scope.tileStatusList = [];
-
-	$scope.success  = {counter: 0};
-
-	$scope.timer = {active: false};
-
-    $scope.revealSuccess = function (success) {
-        if (success) {
-			$scope.userMessage = game.message.success;
-		} else {
-			$scope.userMessage = game.message.fail;
-			var index;
-
-			for (var i=0; i<9; i++) {
-				index = game.secretSelectionList[i];
-				if (!$scope.tileStatusList[index].flash) {
-					$scope.tileStatusList[index].isRevealed = true;
-				}
-			}
-		}
-    };
-
-	/***************************/
-	/* Extend scope with functionalities not bound to view*/
-	var game = new gameDashboardFactory($scope);
-	game.prepareNewGame();
-
-
-	/***************************/
-	/* Bound functions to view */
-	$scope.userMessage = game.message.init;
-
-    $scope.newRound = function () {
-        if ($scope.timer.active) return;
-        $scope.timer.active = true;
-        game.prepareNewGame();
-        game.generateRandomTiles();
-        game.revealThenHideSelected();
-    };
-
-	$scope.endRound = function () {
-        if ($scope.timer.active) return;
-		game.prepareNewGame();
-        $scope.userMessage = game.message.restart;
-    };
+	$scope.game = new Game();
 
 });

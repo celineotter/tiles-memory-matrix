5973013204008f102a0e07fae11616e8f2fd468a
celineotter <celine.otter@gmail.com>
2015-05-25 20:50:50 -0700

    (feat) Add Karma required plugins

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
@@ -66,6 +66,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    plugins: [
+        // Karma will require() these plugins
+        'karma-mocha', 'karma-spec-reporter', 'karma-chrome-launcher'
+    ]
   });
 };
